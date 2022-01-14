ebf59115d7da7ebb1e6cd61b25fc7a90bb2d6529
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2022-01-14 12:22:20 +0000

    modified code in server.js and downloaded dependencies

---
+++ db/seeds/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO vendors (id, name) VALUES (1, 'Ultimate');
+INSERT INTO vendors (id, name) VALUES (2, 'BB Clothing');
+INSERT INTO vendors (id, name) VALUES (3, 'Indian Foods');
+
+INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (1, 'Books', 100, 'Row B', '2020-02-07', 1);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (2, 'Skirts', 250, 'Bin 4', '2020-04-05', 2);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (3, 'Overall', 1000, 'Bin 3', '2020-04-05', 2);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (4, 'Rice', 150, 'Row A', '2021-01-06', 3);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (5, 'Peas', 200, 'Row 2A', '2021-01-10', 3);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (6, 'Pants', 200, 'Bin 2', '2021-02-12', 2);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (7, 'phone device', 100, 'Row B', '2020-02-07', 1)
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const { response } = require('express');
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const port = 3000;
 
-app.get('/', (request, response) => {
-  response.send('Hello Express Here')
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: 'vagrant',
+  host: 'localhost',
+  database: 'logistics'
+});
+
+app.get("/", (request, response) => {
+  response.send("Hello Express Here");
 });
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
-})
+});
