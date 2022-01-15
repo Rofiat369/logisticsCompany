fee922a32d257be6107162f5a35d18500ad0ad2c
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2022-01-15 04:28:31 +0000

    added new routes for inventory

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const db = require("./itemsQueries");
+// const bodyParser = require("body-parser");
+// const db = require("itemsQueries");
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "./views"));
+// app.set("views", path.join(__dirname, "/views"));
 
 // uses body parser globally
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
 
 app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
+  response.render('pages/index', {pageTitle: 'Welcome'})
+  // response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.get("/inventory", (request, response) => {
+  response.render('pages/inventory', {pageTitle: 'Welcome'})
+  // response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
 //ITEMS ROUTES
-app.get("/items", db.getItems);
-app.get("/items/:id", db.getItemById);
-app.post("/items", db.createItem);
-app.put("/items/:id", db.updateItem);
-app.delete("/items/:id", db.deleteItem);
+// app.get("/items", db.getItems);
+// app.get("/items/:id", db.getItemById);
+// app.post("/items", db.createItem);
+// app.put("/items/:id", db.updateItem);
+// app.delete("/items/:id", db.deleteItem);
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
