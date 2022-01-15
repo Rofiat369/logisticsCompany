e49a867f09d015b841f01d151ab5ba9576f9c684
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2022-01-15 22:09:20 +0000

    Added items to the table

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -1,11 +1,22 @@
 INSERT INTO vendors (id, name) VALUES (1, 'Ultimate');
 INSERT INTO vendors (id, name) VALUES (2, 'BB Clothing');
 INSERT INTO vendors (id, name) VALUES (3, 'Indian Foods');
+INSERT INTO vendors (id, name) VALUES (4, 'Initial Drawings Limited');
+INSERT INTO vendors (id, name) VALUES (5, 'D Face');
 
-INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (1, 'Books', 100, 'Row B', '2020-02-07', 1);
-INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (2, 'Skirts', 250, 'Bin 4', '2020-04-05', 2);
-INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (3, 'Overall', 1000, 'Bin 3', '2020-04-05', 2);
-INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (4, 'Rice', 150, 'Row A', '2021-01-06', 3);
-INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (5, 'Peas', 200, 'Row 2A', '2021-01-10', 3);
-INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (6, 'Pants', 200, 'Bin 2', '2021-02-12', 2);
-INSERT INTO items (id, name, Item_quantity, location, Created_at, vendor_id) VALUES (7, 'phone device', 100, 'Row B', '2020-02-07', 1)
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (1, 'Books', 100, 'Row B', '2020-02-07', 10000, 1);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (2, 'Skirts', 250, 'Bin 4', '2020-04-05', 2000, 2);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (3, 'Overall', 1000, 'Bin 3', '2020-04-05', 15000, 2);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (4, 'Rice', 150, 'Row A', '2021-01-06', 13000, 3);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (5, 'Peas', 200, 'Row 2A', '2021-01-10', 5000, 3);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (6, 'Pants', 200, 'Bin 2', '2021-02-12', 4000, 2);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (7, 'phone device', 100, 'Row B', '2020-02-07', 20000, 1);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (8, 'Hijab', 1000, 'Bin 1', '2020-04-01', 15000, 5);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (9, 'niqab', 10000, 'Bin 16', '2020-04-01', 12000, 5);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (10, 'pencils', 15000, 'Row A','2021-02-07', 11000, 4);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (11, 'Color Pencils', 10000, 'Row A', '2021-02-07', 5000, 4);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (12, 'phone cover', 100, 'Row B', '2021-03-07', 4000, 1);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (13, 'perfume', 10000, 'Row D', '2021-04-10', 2000, 5);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (14, 'hand lotion', 10000, 'Row 2D', '2021-04-10', 2000, 5);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (15, 'Pencil Case', 10000, 'Row ', '2021-05-02', 5000, 4);
+INSERT INTO items (id, name, Item_quantity, location, Created_at, total, vendor_id) VALUES (16, 'head phones', 1500, 'Row A', '2021-06-06', 30000, 1);
