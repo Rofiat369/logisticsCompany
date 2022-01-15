0b296c5aed3e0df89f5084f9cbd9bd76f7232a6b
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2022-01-15 01:01:00 +0000

    modified the routes inside server.js

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const db = require("./itemsQueries");
 const app = express();
-
 const port = 3000;
 
-const { Pool } = require('pg');
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
 
-const pool = new Pool({
-  user: 'vagrant',
-  password: 'vagrant',
-  host: 'localhost',
-  database: 'logistics'
-});
+// uses body parser globally
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (request, response) => {
-  response.send("Hello Express Here");
+  response.json({ info: "Node.js, Express, and Postgres API" });
 });
+
+//ITEMS ROUTES
+app.get("/items", db.getItems);
+app.get("/items/:id", db.getItemById);
+app.post("/items", db.createItem);
+app.put("/items/:id", db.updateItem);
+app.delete("/items/:id", db.deleteItem);
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
+});
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "vagrant",
+  host: "localhost",
+  database: "logistics",
 });
