39dbe6d6f2538754fd12c56ae1115dc5156dc868
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2022-01-16 04:31:09 +0000

    created a delete.js file that holds the delete function

--- public/scripts/delete.js
+++ public/scripts/delete.js
@@ -1,10 +1,27 @@
-$(".remove").on('click', function(){
-const confirmation = confirm('Are you sure you want to delete this row?')
+$(function() {
+  $(".deletebtn").on("click", function (event) {
+    let response = prompt("Why do you want to delete?");
+    if(response !== null){
+      if(response.length < 1){
+        alert("Please enter comments")
+      } else {
+        const id = $(this).attr("id");
+        event.preventDefault();
+        $(this).closest("tr").remove();
+        $.ajax({
+          url: `/delete/${id}`,
+          type: "GET",
+          success: function(data){
+    console.log(data.items);
+          },
+          error: function(err){
+            console.log(err)
+          }
+        })
+      }
+    }
+    
+  });
+})
 
-if(confirmation){
-  deleteItems();
-} else {
-  return false;
-}
-  }
-)
+
