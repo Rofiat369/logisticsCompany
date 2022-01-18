770f7ed6083dba2dae5473143edc75fb298ee1c4
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2022-01-18 02:30:18 +0000

    removed the console.logs

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -5,6 +5,7 @@ const getItems = (db) =>{
   SELECT  items.id AS identity, Items.name AS item_name, items.item_quantity, items.location, items.created_at, items.vendor_id, items.total AS Amount, vendors.id, vendors.name AS vendors_name FROM items 
   JOIN vendors ON items.vendor_id = vendors.id
   WHERE items.status = $1
+  ORDER BY identity
   `, [0])
   .then(data => 
     {
